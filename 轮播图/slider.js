8ecf70c006516334d761e28eb6e676e001eb3cfd
ZWkang <907747874@qq.com>
2017-07-13 17:33:32 +0800

    simple add

--- 轮播图/slider.js
+++ 轮播图/slider.js
@@ -11,16 +11,42 @@
 				this.clickdelay = false	//点击是否开启延迟
 				this.animate_duration = '1s' //动画变换时间
 				this.auto_width = true	//是手动输入每个元素块的大小(这种情况下通常可以用作按需加载)还是自动获取
-				this.slider_like = "CSS" //动画函数的效果 是incss or in javascript
+				this.slider_like = "CSS" //动画函数的效果 是css control  or  javascript control
 				this.MODE_SELECT = 1 //三种模式  简单滑动 透明度  无缝滑动
-
+				this.hooks = {}
 
 				this.init=function(){
-					wrap(initPreBtn)(option["pics_pre"])&&
+					
 				}
-				
-				function initContainer(){
-
+				this.hook = function(name,fn){
+					var arr = tihs.hook[name]||tihs.hook = [] &&tihs.hook[name]
+					arr.push(fn)
+				}
+				this.firehook = function(name){
+					var fn = null
+					while(this.firehook[name]!==void 0&&this.firehook[name].length !== 0){
+						fn = this.firehook[name].shift()
+						fn&&fn();
+					}
+				}
+				this.removehook = function(name,fn){
+					if(!name)return 
+					var index
+					if(fn){
+						index = this.firehook[name].indexOf(fn)
+						delete this.firehook[name][index]
+					}else{
+						delete this.firehook[name]
+					}
+				}
+				function initContainer(obj){
+					var i 
+					for(i in arr){
+						if(obj.hasOwnProperty(i)){
+							this.ContainerClass[i] = this.getOne(pics_pre)
+						}
+					}
+					i = null
 				}
 				function initIconList(){
 
