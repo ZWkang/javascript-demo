7173fdad10ba0526f4f4d8343a3f1a98e0dc9036
ZWkang <907747874@qq.com>
2017-07-14 22:39:06 +0800

    update RegExp demo

--- utils/RegExp.test.js
+++ utils/RegExp.test.js
@@ -2,4 +2,22 @@
 * 这是一个用来练习正则的小仓库
 */
 
-
+//千分号
+function parseToMoney(money){
+	if(Number(money)!==money)throw new TypeError('error type')
+	var str = money.toString()
+	var temp =   str.split('.')
+	var _temp = temp[0].split('').reverse().join('')
+	while(/\d{4}?/.test(_temp)){
+		_temp = _temp.replace(/(\d{3})(?=\d)/,function(word){
+			return RegExp.$1+','
+		})
+	}
+	// console.log(_temp)
+	if(!!temp[1]){
+		temp[0] =  _temp.split('').reverse().join('')
+	}
+	
+	return temp.join('.')
+}
+console.log(parseToMoney(13112111123))
