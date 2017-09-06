ecc3545bbf26349a7cade1500c76b9e0c27c3cc0
ZWkang <907747874@qq.com>
2017-09-06 23:47:00 +0800

    thinking how to do it
    现在研究应该如何使用，如果需要使用多个编辑器的时候，则document.execCommand分离则需要iframe的支持，而此时我们必须在外部可控iframe而不是嵌入式地使用src外联style，而应该生成的时候使用它，使用外部传入的参数。而此时在考虑传入参数的可变性，还有就是这种嵌入iframe的做法，如果在vue，在react之间嵌入程度会是怎么样，虽然这是个使用原生api的。。但是还是得考虑这种问题。。

--- 编辑器/Editor.js
+++ 编辑器/Editor.js
@@ -9,71 +9,71 @@
 		root.kangEditor = factory(root);
 	}
 })(this, function (root) {
-    let doc = document
+    const doc = document
     const Action = {
-        backColor :{//修改文档的背景颜色
-            title : "backColor",
-            execCommand : str=>exec('backColor',false,str)
+        backColor: {//修改文档的背景颜色
+            title: "backColor",
+            execCommand: str => exec('backColor', false, str)
         },
         bold: {//字体加粗
-            title : 'bold',
-            execCommand : ()=>exec('bold',false,null)
+            title: 'bold',
+            execCommand: () => exec('bold', false, null)
         },
-        contentReadOnly : {//设置可编辑性
-            title : 'contentReadOnly',
-            execCommand : (bool) => exec('contentReadOnly',false,!!bool)
+        contentReadOnly: {//设置可编辑性
+            title: 'contentReadOnly',
+            execCommand: (bool) => exec('contentReadOnly', false, !!bool)
         },
-        copy : {//拷贝当前选中内容到剪贴板
-            title : 'copy',
-            execCommand : ()=>exec('copy',false,null)
+        copy: {//拷贝当前选中内容到剪贴板
+            title: 'copy',
+            execCommand: () => exec('copy', false, null)
         },
-        createLink : {//将选中内容创建为一个锚链接
-            title : 'createLink',
-            execCommand : ()=>{
+        createLink: {//将选中内容创建为一个锚链接
+            title: 'createLink',
+            execCommand: () => {
                 let link = window.prompt('enter a url link')
-                return exec('createLink',false,link)
+                return exec('createLink', false, link)
             }
         },
-        cut : {// 剪贴当前选中的文字并复制到剪贴板
-            title : 'cut',
-            execCommand:()=>exec('cut',false,null)
+        cut: {// 剪贴当前选中的文字并复制到剪贴板
+            title: 'cut',
+            execCommand: () => exec('cut', false, null)
         },
-        decreaseFontSize : {// 给选中文字加上 <small> 标签
-            title : 'decreaseFontSize',
-            execCommand:()=>exec('decreaseFontSize',false,null)
+        decreaseFontSize: {// 给选中文字加上 <small> 标签
+            title: 'decreaseFontSize',
+            execCommand: () => exec('decreaseFontSize', false, null)
         },
-        delete : {//删除选中部分
-            title : 'delete',
-            execCommand:()=>exec('delete',false,null)
+        delete: {//删除选中部分
+            title: 'delete',
+            execCommand: () => exec('delete', false, null)
         },
-        fontName : {//在插入点或者选中文字部分修改字体名称 需要提供一个字体名称字符串 (例如："Arial")作为参数。
-            title : 'fontName',
-            execCommand:(fontName)=>exec('fontName',false,fontName)
+        fontName: {//在插入点或者选中文字部分修改字体名称 需要提供一个字体名称字符串 (例如："Arial")作为参数。
+            title: 'fontName',
+            execCommand: (fontName) => exec('fontName', false, fontName)
         },
-        fontSize : {//在插入点或者选中文字部分修改字体大小.
-            title : 'fontSize',
-            execCommand:(foreColor)=>exec('fontSize',false,foreColor)
+        fontSize: {//在插入点或者选中文字部分修改字体大小.
+            title: 'fontSize',
+            execCommand: (foreColor) => exec('fontSize', false, foreColor)
         },
-        foreColor : {//在插入点或者选中文字部分修改字体颜色.
-            title : 'foreColor',
-            execCommand:(ColorString)=>exec('foreColor',false,ColorString)
+        foreColor: {//在插入点或者选中文字部分修改字体颜色.
+            title: 'foreColor',
+            execCommand: (ColorString) => exec('foreColor', false, ColorString)
         },
-        forwardDelete : {//删除光标所在位置的字符。 和按下删除键一样。
-            title : 'forwardDelete',
-            execCommand:()=>exec('forwardDelete',false,null)        
+        forwardDelete: {//删除光标所在位置的字符。 和按下删除键一样。
+            title: 'forwardDelete',
+            execCommand: () => exec('forwardDelete', false, null)
         },
-        insertHorizontalRule : {//一个分割行  在插入点插入一个水平线
-            title : 'insertHorizontalRule',
-            execCommand:()=>exec('insertHorizontalRule',false,null)        
+        insertHorizontalRule: {//一个分割行  在插入点插入一个水平线
+            title: 'insertHorizontalRule',
+            execCommand: () => exec('insertHorizontalRule', false, null)
         },
-        insertImage : {//插入图片
-            title : 'insertHorizontalRule',
+        insertImage: {//插入图片
+            title: 'insertHorizontalRule',
             execCommand:()=>{
                 let ImageUrl = window.prompt('enter a image link')
                 exec('insertHorizontalRule',false,ImageUrl)
             }    
         },
-        underline : {
+        underline : {//下划线
             title : 'underline',
             state : true,
             execCommand:()=>exec('underline',false,null)
@@ -97,7 +97,7 @@
             title : 'insertText',
             execCommand:()=>exec('insertParagraph',false,null)
         },
-        italic:{
+        italic:{//斜体
             title : 'italic',
             execCommand:()=>exec('italic',false,null)
         },
@@ -152,7 +152,7 @@
 　　　　　　　selectedText = selectionObj.toString();
 　　　　　　　rangeObj = selectionObj.getRangeAt(0);
 　　　　　　　var docFragment = rangeObj.cloneContents();
-　　　　　　   var tempDiv = document.createElement("div");
+　　　　　　  var tempDiv = document.createElement("div");
 　　　　　　　tempDiv.appendChild(docFragment);
 　　　　　　　selectedHtml = tempDiv.innerHTML;
 　　　　　}else if(document.selection){
@@ -168,5 +168,242 @@
     }
     function EditorWrap(){
 
+    }
+	var objectToString = Object.prototype.toString;
+    var fnToString = Function.prototype.toString;
+
+    //检查类型
+    const checkType = (type)=>{
+        return (obj) => {
+            return objectToString.call(obj) === '[object '+ type +']';
+        }
+    }
+    const ObjectCheckType = checkType('object');
+    const ArrayCheckType = checkType('array');
+    const testDiv = document.createElement('div');
+
+    //检查css属性是否存在
+    const checkCss = (str)=>{
+        if(ArrayCheckType(str)){
+            return str.some((v)=>{
+                return (v in testDiv.style);
+            })
+        }
+        return (str in testDiv.style);
+    }
+    
+    // var cssChange = checkCss('cssChange')
+    /**
+     * Utils
+     */
+    const Utils = {
+        'cssChange':checkCss('cssChange'),
+        'ObjectCheckType' : checkType('Object'),
+        'ArrayCheckType' : checkType('Array'),
+        'isUndefined': checkType('Undefined')
+    }
+    // function generatorIs(){}
+
+    /**
+     * 隔离化操作
+     */
+
+    //定义一些标签模板
+    const iframeTemplate = document.createElement('iframe');
+    const divTemplate = document.createElement('div');
+    const ulTemplate = document.createElement('ul');
+
+
+    class initActionConfig{
+        constructor(ActionList){
+            this.ActionList = ActionList;
+        }
+    }
+    class initStyleConfig{
+        constructor(){
+            this.iframeObject = iframeTemplate.cloneNode();
+        }
+        initIframeStyle(){
+
+        }
+        initContentStyle(){}
+        initTitleStyle(){}
+
+    }
+    // function generatorIframe(setting){
+    class generatorIframe{
+        get iframeObject(){
+
+            return iframeTemplate.cloneNode()
+        }
+        
+        constructor(iframeObject){
+            //默认style样式
+            this.style = {
+                iframe:{
+                    body:{
+                        margin:'0',
+                        padding:'0',
+                        overflow:'hidden'
+                    }
+                },
+                content:{
+                    MaxWidth:'100%',
+                    MinWidth:'100%',
+                    MaxHeight:'100%',
+                    MinHeight:'500px',
+                    FontSize:'16px'
+                },
+                title:{
+                    MaxWidth:'100%',
+                    MinWidth:'100%',
+                    MaxHeight:'20px',
+                    MinHeight:'20px',
+                    FontSize:'16px'
+                },
+                titleitem:{
+                    MaxWidth:'20px',
+                    MinWidth:'20px'
+                }
+            }
+            this.toolbarList = {
+
+            }
+            //挂载的回调函数
+            this.MountFunction = function(){}
+            this.iframeObject = iframeObject
+            this.defaultTitleItems = {
+                bold,
+                insertImage,
+                italic,
+                justifyCenter,
+                htitle:([1,2,3,4,5,6].map((v)=>{
+                        let result = {};
+                        result['title'] = 'H'+v;
+                        result['execCommand'] = ()=>{
+                            Action['formatBlock'].execCommand('h'+v);
+                        }
+                        return result
+                    })),
+                justify:(['Center','Full','Left','Right'].map((v)=>{
+                    return Action['justify'+v];
+                })),
+                insertImage,
+                underline,
+                undo
+            }
+        }
+        get settingsKeys(){
+            return Object.keys(this.defaultTitleItems);
+        }
+        
+        generatorUlList(nums,event,text){
+            if(Utils.ArrayCheckType(nums)){
+                console.error(`error nums argument`);
+                return false;
+            }
+            let ulContainer = ulTemplate.cloneNode();
+            let liContainer = document.createElement('li');
+            for (let i = 0;i < nums;i++){
+                liContainer.onclick = event||'';
+                liContainer.innerText = text;
+                if(Utils.ArrayCheckType(nums[i])){
+                    ulContainer.appendChild(generatorUlList(nums[i]))
+                }else{
+                    ulContainer.appendChild(liContainer.cloneNode())
+                }
+            }
+            return ulContainer;
+        }    
+        initConfig(setting){
+            if(Utils.ObjectCheckType[setting]&&Utils.isUndefined(setting['style'])){
+                console.error('setting throw a typeerror');
+                return ;
+            }
+            const style = setting.style;
+            styleGiveChange(this.style,style);
+
+        }
+        static titleItemFixSetting(defaultsetting,setting){
+            if(Utils.isUndefined(setting)){
+                console.error('maybe you want default setting')
+                return false;
+            }
+            const keys = Object.keys(setting);
+            for(let i in keys){
+                if(Utils.ArrayCheckType(i)){
+                    if(i in this.settingsKeys){
+
+                    }
+                }else{
+                    
+                }
+            }
+
+        }
+        static styleGiveChange(style,setting){
+            if(!(this instanceof generatorIframe)){
+                console.error('plz use new generatorIframe');
+                return ;
+            }
+            const styleKeys = Object.keys(style);
+            styleKeys.forEach((v,index)=>{
+                this.style[v] = Object.assign(this.style[v],setting[v]);
+            })
+        }
+        
+        clearDefault (eleObject){
+            eleObject.style.margin='0';
+            eleObject.style.padding = '0';
+            return eleObject;
+        }
+        static giveValue(target = {},result){
+            for(let i in target){
+                if(target.hasOwnProperty(i)){
+                    result[i] = target[i];
+                }
+            }
+            return result
+        }
+        static generatorContent(){
+            let divObject = divTemplate.cloneNode();
+            divObject.setAttribute('contenteditable','true');
+            return this.giveValue(this.style['content'],divObject);
+        }
+        
+        static generatorTitle(){
+            let divObject = divTemplate.cloneNode()
+            // divObject.appendChild()
+        }
+
+
+        generatorDefaultIframe(){
+
+        }
+        // return iframeObject;
+    }
+    /**
+     * 数据绑定接口
+     */
+    
+    function bangInterface(eleObject){
+        var saveCache = {}
+        return function(context){
+            var arg = arguments.slice(1);
+            // eleObject.addEventListener(event,callback,false);
+            if(!!saveCache[arg[0]]){
+                eleObject.removeEventListener.apply(context,arg);
+            }
+            saveCache[arg[0]] = arg[1];
+            eleObject.addEventListener.apply(context,arg);
+            return true;
+        }
+    }
+    
+    /*
+     * 初始化操作
+     */
+    function init(mountElementObject){
+    
     }
 })
